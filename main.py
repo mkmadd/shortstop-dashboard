f0ddf86c1a0330236ceeacd73cd962385c5561b4
mkmadd <mkmadd@gmail.com>
2015-07-30 20:03:43 -0500

    Add OAuth2 authorization after discontinuation of gdata; fix timezones

--- main.py
+++ main.py
@@ -6,21 +6,33 @@ Created on Tue Jan 13 14:42:16 2015
 """
 
 import bottle
-import sys, os
 import re
-import gdata.docs.client, gdata.client
 import logging
+from codecs import BOM_UTF8
 from natsort import natsorted
 from itertools import groupby
 from datetime import datetime, timedelta
+from pytz import timezone, utc
+
+import httplib2
+
+from apiclient import discovery, errors
+import oauth2client
+from oauth2client import client
+from oauth2client import tools
+from oauth2client.appengine import CredentialsNDBModel, StorageByKeyName
 
 # had to change from <span> to <span class="c."> when Google docs suddenly 
 # changed.  For some reason the class changes from c0 to c2
 ROW_RE = re.compile(r'<span class="c.">(.*?)</span>')
 ALARM_DOC_NAME = 'Alarm Data' # google doc that stores alarm data
 INV_DOC_NAME = 'Inventory Data' # google doc that stores inventory data
-username = os.environ['GOOG_UID'] # google/gmail login id
-password = os.environ['GOOG_PWD'] # google/gmail login password
+
+SCOPES = 'https://www.googleapis.com/auth/drive.readonly'
+CLIENT_SECRET_FILE = 'client_secret.json'
+APPLICATION_NAME = 'Shortstop Dashboard'
+ALARM_FILE_ID = '1kQ9Bfs7S-hd_NiPTZa0JQLYWg0FNVNgGl-9yRe3fI0c'
+INV_FILE_ID = '1FfsSGg2XUExzPmggwvKSxRqG6sUnau7Ik53CSocKNjM'
 
 EXPIRED_TIME = 20   # Number of minutes after which a time is expired
 
@@ -113,54 +125,79 @@ WARNING_LEVELS = {
     }
 }
 
+def get_credentials():
+    """Gets valid user credentials from storage.
 
+    If nothing has been stored, or if the stored credentials are invalid,
+    the OAuth2 flow is completed to obtain the new credentials.
 
-def load_docs():
-#    logging.info('entering load_docs...')
-    client = gdata.docs.client.DocsClient(source='shortstop-dash')
-    client.ssl= True
+    Returns:
+        Credentials, the obtained credential.
+    """
+#    home_dir = os.path.expanduser('~')
+#    credential_dir = os.path.join(home_dir, '.credentials')
+#    if not os.path.exists(credential_dir):
+#        os.makedirs(credential_dir)
+#    credential_path = os.path.join(credential_dir,
+#                                   'shortstop-dash.json')
+
+    # Create Storage object associated with NDB datastore and get credentials
+    storage = StorageByKeyName(CredentialsNDBModel, 
+                               'shortstop_cred', 'credentials')
+    credentials = storage.get()
     
-    try:
-        client.ClientLogin(username, password, client.source);
-    except (gdata.client.BadAuthentication, gdata.client.Error), e:
-        logging.error('Could not load docs: {}'.format(e))
-#        sys.exit('ERROR: ' + str(e))
-    except:
-        logging.error('ERROR: Unable to login')
-#        sys.exit('ERROR: Unable to login')
-    return client
+    # If credentials not there, try to load from starter file
+    if not credentials:
+        store = oauth2client.file.Storage('shortstop-dash.json')
+        credentials = store.get()
+        storage.put(credentials)
+        credentials = storage.get()
 
-def retrieve_doc(document_name):
-#    logging.info('entering retrieve_doc...')
-    client = load_docs()
-#    logging.info('retrieving resources...')
-    doc_list = client.get_resources()
-    docs = []
-    for i in doc_list.entry:
-        docs.append(i.title.text)
-    doc_num = None
-    for i, j in enumerate(docs):
-        if j == document_name:
-            doc_num = i
-    if doc_num == None:
-        return 0
-        
-    entry = doc_list.entry[doc_num]
-#    logging.info('downloading to memory...')
-    content = client.download_resource_to_memory(entry)
-    rows = ROW_RE.findall(content)
-    new_rows = []
-    if rows:
-#        logging.info(rows[0])
-        for row in rows:
-            # after fixing <span class...>, had to filter out empty rows
-            if row == '':
-                continue
-            new_row = tuple(row.split('|'))
-            new_rows.append(new_row)
-    else:
-        logging.error('no rows')
-    return new_rows
+    if not credentials or credentials.invalid:
+        flow = client.flow_from_clientsecrets(CLIENT_SECRET_FILE, SCOPES)
+        flow.user_agent = APPLICATION_NAME
+        flags = tools.argparser.parse_args(args=[])
+        credentials = tools.run_flow(flow, storage, flags)
+        if not credentials:
+            logging.error('Could not obtain valid credentials')
+
+#    storage.put(credentials)
+
+    return credentials
+
+
+def retrieve_doc(file_id):
+    # Get credentials to access shortstop.dash account
+    credentials = get_credentials()
+    
+    # Access shortstop.dash Google Drive
+    http = httplib2.Http()
+#    http = credentials.refresh(http)
+    http = credentials.authorize(http)
+    service = discovery.build('drive', 'v2', http=http)
+
+    # Some code for listing all files and their ids in Google Drive
+#    results = service.files().list().execute()
+#    items = results.get('items', [])
+#    for item in items:
+#        logging.info('{0} ({1})'.format(item['title'], item['id']))
+
+    # ...and try to get the meta info for the file we want
+    try:
+        file_meta = service.files().get(fileId=file_id).execute()
+    except errors.HttpError, error:
+        logging.error('An error occurred trying to get file: {}'.format(error))
+
+    # Get the url for the file from its metadata, download it, and decode it
+    dl_url = file_meta['exportLinks']['text/plain']
+
+    resp, content = service._http.request(dl_url)
+    content = content.lstrip(BOM_UTF8).decode('utf-8')
+    
+    # Data is pipe-delimited; split it up and format it into rows of tuples
+    rows = [tuple(row.split('|')) for row in content.split('\r\n')]
+
+    return rows
 
 
 def fix_name(name):
@@ -197,15 +234,20 @@ def format_stores(rows):
             new_tank['storage_id'] = tank[1]
             new_tank['tank_name'] = fix_name(tank[8])
             new_tank['product_name'] = fix_name(tank[9])
-            new_tank['gross_volume'] = int(float(tank[3])) if tank[3] != 'None' else None
-            new_tank['ullage'] = int(float(tank[4])) if tank[4] != 'None' else None
-            new_tank['last_updated'] = datetime.strptime(tank[7], '%Y-%m-%d %H:%M:%S')
+            new_tank['gross_volume'] = int(float(tank[3])) \
+                                       if tank[3] != 'None' else None
+            new_tank['ullage'] = int(float(tank[4])) \
+                                 if tank[4] != 'None' else None
+            new_tank['last_updated'] = datetime.strptime(tank[7], 
+                                                         '%Y-%m-%d %H:%M:%S')
             new_tank['tank_low'] = False
             if name != 'Shortstop 25':
-                warn_lvl = WARNING_LEVELS[name].get(new_tank['product_name'], 0)
+                warn_lvl = WARNING_LEVELS[name].get(new_tank['product_name'], 
+                                                    0)
             elif '1' not in new_tank['tank_name'] and \
                  '2' not in new_tank['tank_name']:
-                warn_lvl = WARNING_LEVELS[name].get(new_tank['product_name'], 0)
+                warn_lvl = WARNING_LEVELS[name].get(new_tank['product_name'],
+                                                    0)
             else:
                 warn_lvl = 0
             if new_tank['gross_volume'] <= warn_lvl:
@@ -213,16 +255,20 @@ def format_stores(rows):
             tank_info.append(new_tank)
             
         # Find the row with the earliest time to use at the last update
-        earliest = min(tank_info, key=lambda x: x['last_updated'])
-        store['last_update_time'] = earliest['last_updated'].strftime('%I:%M %p')
-        store['last_update_date'] = earliest['last_updated'].strftime("%b %d, '%y")
+        earliest_row = min(tank_info, key=lambda x: x['last_updated'])
+        central = timezone('US/Central')
+        earliest = central.localize(earliest_row['last_updated'])
+        store['last_update_time'] = earliest.strftime('%I:%M %p')
+        store['last_update_date'] = earliest.strftime("%b %d, '%y")
         
+        # Google App Engine time is UTC - adjust to CST
+        adjusted_now = utc.localize(datetime.now()).astimezone(central)
         # If old date/time, set expired flags
-        if earliest['last_updated'].date() != (datetime.now() - timedelta(hours=6)).date():
+        if earliest.date() != adjusted_now.date():
             store['date_expired'] = True
         else:
             store['date_expired'] = False
-        if (earliest['last_updated'] + timedelta(minutes=EXPIRED_TIME)) < datetime.now() - timedelta(hours=6):
+        if (earliest + timedelta(minutes=EXPIRED_TIME)) < adjusted_now:
             store['time_expired'] = True
         else:
             store['time_expired'] = False
@@ -235,23 +281,20 @@ def format_stores(rows):
 @bottle.route('/')
 @bottle.view('main')
 def main():
-    rows = retrieve_doc(INV_DOC_NAME)
-#    logging.info('got rows...')
+    rows = retrieve_doc(INV_FILE_ID)
     rows = natsorted(rows, key=lambda x: x[-1])
-#    logging.info('sorted rows...')
-    alarms = retrieve_doc(ALARM_DOC_NAME)
-#    logging.info('got alarms...')
-#    logging.info(rows[:5])
+    alarms = retrieve_doc(ALARM_FILE_ID)
     stores = format_stores(rows)
-#    logging.info('formatted stores...')
     return { 'stores': stores, 'alarms': alarms }
+
     
 @bottle.route('/alarms')
 @bottle.view('alarm')
 def show_alarms():
-    alarms = retrieve_doc(ALARM_DOC_NAME)
+    alarms = retrieve_doc(ALARM_FILE_ID)
     alarms = natsorted(alarms, key=lambda x: x[0])
     #alarms = format_alarms(alarms)
     return { 'alarms': alarms }
 
 app = bottle.default_app()
+bottle.debug(True)
